264967fe5b922006ba7b3d07106e61344c79ce56
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-04-22 17:52:36 +0200

    22-04-2023

---
+++ src/components/App/App.jsx
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+
+import SearchForm from '../Searchbar/Searchbar';
+import { Gallery } from '../ImageGallery/ImageGallery';
+import { LoadMoreButton } from '../Button/Button';
+import { LoaderSpinner } from '../Loader/Loader';
+import { Modal } from '../Modal/Modal';
+import { LoadingPage } from './AppStyle';
+
+const API_KEY = '34314700-bbc151dc7cc4dd7fb1386aaa3';
+
+export default class App extends Component {
+  state = {
+    images: null,
+    isLoading: false,
+    query: '',
+    status: 'idle',
+    page: 1,
+    largeImageURL: '',
+    showModal: false,
+    loadMore: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ page: 1, images: null, loadMore: false });
+      this.fetchImages();
+    } else if (prevState.page !== this.state.page) {
+      this.fetchImages();
+    }
+  }
+
+  fetchImages = async () => {
+    const { query, page } = this.state;
+
+    this.setState({ isLoading: true, status: 'pending' });
+
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+
+      const hits = response.data.hits.map(
+        ({ id, largeImageURL, webformatURL }) => ({
+          id,
+          largeImageURL,
+          webformatURL,
+        })
+      );
+
+      if (hits.length === 0) {
+        toast.error(`Ups...we have not ${query} images...`);
+        this.setState({ images: null, status: 'rejected', loadMore: false });
+        return;
+      }
+
+      if (page === 1) {
+        this.setState({ images: hits, status: 'resolved' });
+      } else {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...hits],
+          status: 'resolved',
+        }));
+      }
+
+      if (response.data.totalHits > page * 12) {
+        this.setState({ loadMore: true });
+      } else {
+        this.setState({ loadMore: false });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({ status: 'rejected' });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  resetState() {
+    this.setState({
+      isLoading: false,
+      status: 'idle',
+      page: 1,
+      largeImageURL: '',
+      showModal: false,
+      loadMore: false,
+    });
+  }
+
+  handleFormSubmit = query => {
+    this.setState({ query });
+    this.resetState();
+  };
+
+  handleLoadMoreClick = () => {
+    this.setState({ isLoading: true });
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleImageClick = largeImageURL => {
+    this.setState({ showModal: true, largeImageURL });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const {
+      images,
+      isLoading,
+      status,
+      showModal,
+      largeImageURL,
+      query,
+      loadMore,
+    } = this.state;
+
+    if (status === 'idle') {
+      return <SearchForm onSubmit={this.handleFormSubmit} value={query} />;
+    }
+
+    if (status === 'pending' && images === null) {
+      return (
+        <>
+          <SearchForm onSubmit={this.handleFormSubmit} value={query} />
+          <LoadingPage>
+            <LoaderSpinner />
+          </LoadingPage>
+        </>
+      );
+    }
+
+    if (status === 'rejected') {
+      return (
+        <div>
+          <SearchForm onSubmit={this.handleFormSubmit} value={query} />
+          <ToastContainer autoClose={3000} />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <SearchForm onSubmit={this.handleFormSubmit} value={query} />
+
+        {images && (
+          <Gallery images={images} onImageClick={this.handleImageClick} />
+        )}
+
+        {isLoading && (
+          <LoadingPage>
+            <LoaderSpinner />
+          </LoadingPage>
+        )}
+
+        {loadMore && !isLoading && (
+          <LoadMoreButton onClick={this.handleLoadMoreClick} />
+        )}
+
+        {showModal && (
+          <Modal
+            onClose={this.handleCloseModal}
+            largeImageURL={largeImageURL}
+          ></Modal>
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+import { Header, Input, Button } from './SearchbarStyle';
+
+export default class SearchForm extends Component {
+  state = {
+    inputQuery: '',
+  };
+
+  handleQueryChange = event => {
+    this.setState({ inputQuery: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.inputQuery.trim() === '') {
+      return toast.error('Please, type something...');
+    }
+
+    const { inputQuery } = this.state;
+    inputQuery.toLowerCase().trim();
+    this.props.onSubmit(inputQuery);
+  };
+
+  render() {
+    const { inputQuery } = this.state;
+    return (
+      <Header>
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+        >
+          <Button type="submit">
+          </Button>
+          <Input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleQueryChange}
+            value={inputQuery}
+          />
+        </form>
+      </Header>
+    );
+  }
+}
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
